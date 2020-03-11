03b547452d7d6da6c982ee0b62211f38fb091da7
salar384 <salar4info@gmail.com>
2020-03-11 21:08:09 +0530

    aplicatation.properties

--- village-gateway/src/main/java/com/village/gateway/VillageGatewayApplication.java
+++ village-gateway/src/main/java/com/village/gateway/VillageGatewayApplication.java
@@ -2,6 +2,7 @@ package com.village.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VillageGatewayApplication {
+	
+	/*
+	 * @Autowired private LoadBalancerClient loadBalancerClient;
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(VillageGatewayApplication.class, args);
@@ -25,10 +31,18 @@ public class VillageGatewayApplication {
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		//@formatter:off
 		return builder.routes()
-				.route(r -> r.path("/getAnyAddress")
+				.route(r -> r.path("/Address/**")
 						.uri("http://localhost:8000"))
+						//.uri(loadBalancerClient.choose("Address-Eureka-Client").getUri().toString()))
+				.route(r -> r.path("/Order/**")
+						.uri("http://localhost:8002"))
+						//.uri(loadBalancerClient.choose("Order-Eureka-Client").getUri().toString()))
+				.route(r -> r.path("/Product/**")
+						.uri("http://localhost:8001"))
+						//.uri(loadBalancerClient.choose("Product-Eureka-Client").getUri().toString()))
 				.build();
 		//@formatter:on
 	}
 	
 }
+
